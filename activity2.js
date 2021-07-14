f24a2ed9c83ee01c4e018fb8617d4d3f388c2e12
MrAntiTesla <81398108+MrAntiTesla@users.noreply.github.com>
2021-07-13 18:40:05 -0700

    Delete activity2.js

--- activity2.js
+++
@@ -1,9 +0,0 @@
-function back()
-{
-window.location = "activity_1.html";
-}
-function getScore()
-{
-    score = localStorage.getItem("score"); 
-    document.getElementById("Update").innerHTML = "<h1>Score: " + score +"</h1>"
-}
